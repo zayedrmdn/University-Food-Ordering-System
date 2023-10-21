681220e107f25d7f3536b2079a15d047f96500d6
Y7un <116893169+Y7un@users.noreply.github.com>
2023-10-21 15:14:27 +0800

    Delete src/Vendor_SEPEHR directory
    Deleted anomaly files

--- src/Vendor_SEPEHR/VendorClass.java
+++
@@ -1,9 +0,0 @@
-package Vendor_SEPEHR;
-
-public class VendorClass {
-    
-    public static void VendorMenu(){
-        Vendor_Menu menu = new Vendor_Menu();
-        menu.setVisible(true);
-    }    
-}
