e651cbac6cb5803896e1b6cb61c5137b3f42db4c
NaimMeod <zayedrmdn@gmail.com>
2023-11-01 11:43:51 +0800

    Squashed commit of the following:
    commit f3925b71b51fa7458ed040a9c20d55d8f6d14a45
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Fri Oct 27 17:07:07 2023 +0800
    
    commit 41a9bd61a66e595838d75b555f2d2554eaa4d5ac
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Wed Oct 25 20:18:30 2023 +0800
    
    commit fb945838c86a9ebe471cff3514b9a521a5b571ba
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Wed Oct 25 11:49:16 2023 +0800
    
    commit 92be21b95d2c020b69cdcbaf5801dd865bb2fa4a
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Mon Oct 23 11:12:47 2023 +0800
    
    commit 24c163ee7a90050325d2ed26d60b227b54df89e3
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Sat Oct 21 23:03:35 2023 +0800
    
    commit 474eeacbaba97b0be125549ff9e3447ee42c4e57
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Sat Oct 21 16:45:55 2023 +0800
    
    commit cf39015109c327b9f8447bc904efa6f3518a6024
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Sat Oct 21 14:20:46 2023 +0800
    
    commit 281923b161bfa6e2c8692c7aac608260eecbde6c
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Wed Oct 18 09:52:05 2023 +0800
    
    commit 2d46c4c97be2dcc9dab239056c7eead23a63496b
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Tue Oct 17 11:03:09 2023 +0800
    
    commit 3377b1fe2e198575293dec7ac680d5ceb109c3e6
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Sun Oct 15 17:11:28 2023 +0800
    
    commit 10208b43e0b826f5a1ad4ec72663329b8eeb2059
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Sat Oct 14 17:39:17 2023 +0800
    
    commit 5e2f42c8a36b8a422f1ad76a54c74a6171f08e69
    Author: sepehrjokanian <sepehrjokanian@localhost>
    Date:   Fri Oct 13 13:32:22 2023 +0800

---
+++ src/Vendor_SEPEHR/VendorClass.java
@@ -0,0 +1,36 @@
+package Vendor_SEPEHR;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+
+
+public class VendorClass {
+    public static void MenuItem(){
+        MenuItem menu = new MenuItem();
+        menu.setVisible(true);
+    }
+    
+    public static void AcceptOrder(){
+        AcceptOrder ord=new AcceptOrder();
+        ord.setVisible(true);
+    }
+    public static void Order(){
+        Order1 orde=new Order1();
+        orde.setVisible(true);
+    }
+  public static void StatusOrder(){
+      StatusOrder so=new StatusOrder();
+      so.setVisible(true);
+  }
+  public static void RevenueDashh(){
+      RevenueDash dsh=new RevenueDash();
+      dsh.setVisible(true);
+  }
+  public static String generateOrderID(){
+      long timestamp=System.currentTimeMillis();
+      int random=(int)(Math.random()*5);
+      return String.format("%d",timestamp,random,UUID.randomUUID());
+  }
+}
