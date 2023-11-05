6c513d487a3b746020582916157fac30c66a06f4
yjun0 <yjun0@YJ>
2023-11-05 23:00:41 +0800

    Put all the files handling parts into class

--- src/Yjun/Order.java
+++ src/Yjun/Order.java
@@ -4,11 +4,19 @@
  */
 package Yjun;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 
 public class Order {
@@ -17,21 +25,101 @@ public class Order {
     private List<Object> prices;
     private List<String> status;
 
+
     public Order() {
         this.items = new ArrayList<>();
         this.reviews = new ArrayList<>();
         this.prices = new ArrayList<>();
         this.status = new ArrayList<>();
     }
+    
+    public void CancelOrder(JComboBox<String> comboBox,String selectedOrderID)
+    {
+        String workingDirectory = System.getProperty("user.dir");
+        File file = new File(workingDirectory + "/src/Yjun/resources/Status.txt");
+        File tempFile = new File(workingDirectory + "/src/Yjun/resources/TempOrder.txt");
 
-    public void saveOrderToFile(String filename) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
-            for (int i = 0; i < items.size(); i++) {
-                String line = "Food:" + items.get(i) + "\nPrices:" + prices.get(i) + "\n";
+        try (BufferedReader reader = new BufferedReader(new FileReader(file)); BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Order ID:") && line.substring(10).equals(selectedOrderID)) {
+                    String nextLine = reader.readLine(); 
+                    if (!selectedOrderID.contains("Progressing") || !selectedOrderID.contains("Delivered")) {
+                        continue;
+                        
+                    } else {
+   
+                        System.out.println("Order with status 'Progressing' or 'Delivered' cannot be canceled.");
+                        writer.write(line);
+                        writer.newLine();
+                        writer.write(nextLine);
+                        writer.newLine();
+                        continue;
+                    }
+                }
+
                 writer.write(line);
+                writer.newLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+
+        if (file.delete() && tempFile.renameTo(file)) {
+
+            comboBox.removeItem(selectedOrderID);
+        } else {
+            System.out.println("Failed to cancel order. You may need to manually replace the files.");
+        }
+    }
+    
+    public void SaveToTable(JTable table)
+    {
+         String workingDirectory = System.getProperty("user.dir");
+        File file = new File(workingDirectory + "/src/Yjun/resources/Orders.txt");
+        
+        try {
+            FileReader Fread=new FileReader(file);
+            BufferedReader Bread=new BufferedReader(Fread);
+            DefaultTableModel md= (DefaultTableModel)table.getModel();
+            Object[] lines=Bread.lines().toArray();
+            
+            for(int i=0;i<lines.length;i++){
+                String[] row=lines[i].toString().split(" ");
+                md.addRow(row);
+            }
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveOrderToFile(JTable table) {
+        String workingDirectory = System.getProperty("user.dir");
+        File file = new File(workingDirectory + "/src/Yjun/resources/Orders.txt");
+        try {
+            FileWriter fw=new FileWriter(file,true);
+            BufferedWriter bw=new BufferedWriter(fw);
+            for(int i=0;i<table.getRowCount();i++){
+                Date currentTime=new Date();
+                SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String formattedTime=dateFormat.format(currentTime);
+                bw.write(table.getValueAt(i,0).toString()+" "+table.getValueAt(i,1).toString()+" "+formattedTime);
+                    
+            
+            bw.newLine(); 
+            }
+            bw.close();
+            fw.close();
+
+            System.out.println("Order Ordered");
+            
+        } catch (IOException e) {
+            e.printStackTrace();;
         }
     }
     
