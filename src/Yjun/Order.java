c035f6d9e102a8d1546e803634d29ba9b46a4b96
yjun0 <yjun0@YJ>
2023-11-09 20:49:31 +0800

    Finished all of customer functions

--- src/Yjun/Order.java
+++ src/Yjun/Order.java
@@ -21,14 +21,12 @@ import javax.swing.table.DefaultTableModel;
 
 public class Order {
     private List<Object> items;
-    private List<String> reviews;
     private List<Object> prices;
     private List<String> status;
 
 
     public Order() {
         this.items = new ArrayList<>();
-        this.reviews = new ArrayList<>();
         this.prices = new ArrayList<>();
         this.status = new ArrayList<>();
     }
@@ -125,43 +123,17 @@ public class Order {
         }
     }
     
-    public void appendOrderToFile(String filename) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
-            for (int i = 0; i < items.size(); i++) {
-                String line = "Food:" + items.get(i) + "\nPrices:" + prices.get(i) + "\n";
-                writer.write(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
 
     public void addItem(Object foodName, Object price) {
         items.add(foodName);
         prices.add(price);
     }
 
-    public void placeOrder() {
-        if (items.isEmpty()) {
-            System.out.println("Cannot place an empty order.");
-            return;
-        }
-        status.add("Placed");
-    }
-
-
-
-
     public List<Object> getItems() {
         return items;
     }
     public List<Object> getPrices() {
         return prices;
-    }
-
-    public List<String> getReviews() {
-        return reviews;
     }
 
     public List<String> getStatus() {
