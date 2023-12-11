e128766c2b1d09c7c22fae2de53d507eb08091a9
NaimMeod <zayedrmdn@gmail.com>
2023-12-11 10:48:57 +0800

    Update AdminAccounts.java

--- src/Admin_Zayed/AdminAccounts.java
+++ src/Admin_Zayed/AdminAccounts.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Admin_Zayed;
 
-/**
- *
- * @author Zayed
- */
 public class AdminAccounts {
     
+    private String username;
+    private String password;
+    private String role;
+    private Double balance;
+    
+    public AdminAccounts(String username, String password, String role, Double balance) {
+        
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.balance = balance;
+    }
+
+    // Getter and setter methods for 'username'
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and setter methods for 'password'
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter and setter methods for 'role'
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Getter and setter methods for 'balance'
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
 }
